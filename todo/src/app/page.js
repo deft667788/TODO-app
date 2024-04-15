051c1a9e92b6d5641d3aad5a265a61a7bba599a8
Allen <hanwen.shi667788@gmail.com>
2024-04-15 17:07:02 +0800

    Hard-code

--- todo/src/app/page.js
+++ todo/src/app/page.js
@@ -3,13 +3,24 @@ import Form from "@/components/Form";
 import Header from "@/components/Header";
 import TODOHero from "@/components/TODOHero";
 import TODOList from "@/components/TODOList";
+import React from "react";
+
 function Home() {
+  const [todos, setTodos] = React.useState([
+    { title: "Some task", id: self.crypto.randomUUID(), is_completed: false },
+    {
+      title: "Some other task",
+      id: self.crypto.randomUUID(),
+      is_completed: true,
+    },
+    { title: "last task", id: self.crypto.randomUUID(), is_completed: false },
+  ]);
   return (
     <div className="wrapper">
       <Header />
       <TODOHero todos_completed={0} total_todos={0} />
       <Form />
-      <TODOList todos={[]} />
+      <TODOList todos={todos} />
     </div>
   );
 }
